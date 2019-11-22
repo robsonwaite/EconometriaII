cf41de256c5398f2efe5f4f8bcb637e0b3226257
Robson <Waite>
2019-11-22 00:10:39 -0300

    att 2

--- src/Lista_4.R
+++ src/Lista_4.R
@@ -5,9 +5,11 @@
 #     Descrição: Exercicios                                                    #           
 #<============================================================================>#
 ## Preparando Ambiente #########################################################
-  
+  # Diretorio
   setwd("./inputs/DadosGujarati/") 
-
+  # Pacotes
+  library(tseries)
+  library(lmtest)
 
 # Questão 01 ###################################################################
   # Lendo dados ---
@@ -17,6 +19,7 @@
     summary(fit01)
     # As variaveis I, L e A possuem significancia em 5%. (descrever o restante)
   # B) Obtenha os residuos e os residuos padronizados e faça um grafico 
+      par(mfrow=c(2,1))
       #Residuos 
       plot(tabela12.7$YEAR ,residuals(fit01),
              ylab = "Residuos", # Legenda de y
@@ -27,21 +30,55 @@
              pch= 20, # desenho dos "plots"
              cex= 1  # tamanho dos "pontos" plotados
         )
-    # Pelo Grafico percebe-se 'ausência de padrão sistemático'
       
+      # Residuos Padronizados
+      plot(tabela12.7$YEAR ,rstandard(fit01),
+           ylab = "Residuos", # Legenda de y
+           xlab = "Tempo", # Legenda de Y
+           type = "l", # tipo de grafico - pontos
+           main = "Análise de Resíduos Padronizados", # Titulo do grafico
+           col = "red", # Cor dos Plots
+           pch= 20, # desenho dos "plots"
+           cex= 1  # tamanho dos "pontos" plotados
+      )   
+    # Pelo Grafico percebe-se 'ausência de padrão sistemático'
+  
+  # C) Estatistica Durbin Watson (d) - Fale sobre a natureza da autocorrelação
+    # Calculo do Durbin
+      d1 = lmtest::dwtest(fit01)
+      # >> Com d = 0.95 temos uma evidencia indicando a correlação serial positiva.
     
+  # D) Execute o Run Test
+      
+      resR = as.vector(residuals(fit01))
+      sinaisR = sign(resR)
+      R = length(rle(sinaisR)$lengths)
+      RunsR = rle(sinaisR)
+      N = length(sinaisR)
+      N1 = sum(RunsR$lengths[RunsR$value==1])
+      N2 = sum(RunsR$lengths[RunsR$value==-1])
+      ER = 2*N1*N2/N + 1
+      VR = (4*(N1^2)*(N2^2) - 2*N1*N2*N)/((N^2)*(N-1))
+      sdR = sqrt(VR)
+      
+      du = ER + 1.96*sdR
+      dl = ER - 1.96*sdR
     
+      #  R(9) < dl(10.55) < R() < du(20.91) -> Como R não pertence ao Intervalo Critivo
+      #  Logo estamos diante de Autocorrelação ou correlação serial positiva Confirmando
+      #  o que estava sendo indicado pelo Durbin-Watson .
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  # E) Teorica
+      
 # Questão 02 ###################################################################
+  # Leitura dos dados
+    tabela12.8 = read.table("Table 12.8.txt", skip = 6, header = T, stringsAsFactors = F)
+  # A) Verifique que o d de DW é igual 0.4148
+    d2 = lmtest::dwtest(formula = Y ~ X, data = tabela12.8)
+     # Sim, DW é igual a 0.4148
+  # B) Existe Evidencia de Autocorrelação Positiva ?
+      
+        
 # Questão 03 ###################################################################
 # Questão 04 ###################################################################
  
